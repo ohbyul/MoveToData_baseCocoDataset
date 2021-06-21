d775ef271a8d48403c8b0db8a3a2265609797f3c
ohbyul <dhquf8093@naver.com>
2021-06-21 15:37:45 +0900

    add anotation 6.21

--- src/main/java/com/xiilab/movetofile/MoveToImageAndLabel.java
+++ src/main/java/com/xiilab/movetofile/MoveToImageAndLabel.java
@@ -15,6 +15,16 @@ public class MoveToImageAndLabel {
 	public static int indexlabel= 1;
 	
 
+	/*
+	 * 1. 출발 디렉토리를 뒤져 파일이 나올때까지 루프를 돌린다.
+	 * 2. 이미지파일명과 같은 라벨명 존재하는지 체크
+	 * 3. 중복 방지를 위해 밀리세컨 파일명 추가
+	 * 4. 7대3 비율로 train val로 나눔
+	 * 5. coco dataset 형식에 맞게 이미지와 라벨파일 복사해줌
+	 * 
+	 * 
+	 * */
+	
 	public static void main(String[] args) {
 		
 		String BASE_DATA_PATH = "디텍팅 출발 디렉토리 패쓰";		//가져올 디렉토리
